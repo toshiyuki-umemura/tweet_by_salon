35ac561aa9d012ac2872898c3b6eb129c1c61bec
chokobo-ume <@users.noreply.github.com>
2020-01-17 22:40:14 +0900

    Update production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,8 @@
 server '18.178.18.18', user: 'ec2-user', roles: %w{app db web}
 
+set :rails_env, "production"
+set :unicorn_rack_env, "production"
+
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
