7145af72395a4461457dfb8a6a1bd9751c3adf84
masuzawa1 <57562777+masuzawa1@users.noreply.github.com>
2020-01-21 00:29:57 +0900

    add varidation to tweet_model & table

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,2 +1,4 @@
 class Tweet < ApplicationRecord
+  validates :name, presence: true
+  validates :text, presence: true
 end
--- db/migrate/20200118113144_create_tweets.rb
+++ db/migrate/20200118113144_create_tweets.rb
@@ -1,8 +1,8 @@
 class CreateTweets < ActiveRecord::Migration[5.2]
   def change
     create_table :tweets do |t|
-      t.string :name
-      t.string :text
+      t.string :name, null: false
+      t.string :text, null: false
       t.text :image
       t.timestamps
     end
