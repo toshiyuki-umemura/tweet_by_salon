f76732db88160d1d951dc985e463187c8a5069d5
masuzawa1 <57562777+masuzawa1@users.noreply.github.com>
2020-01-22 21:30:21 +0900

    add space to tweets_migratetion_file

--- db/migrate/20200118113144_create_tweets.rb
+++ db/migrate/20200118113144_create_tweets.rb
@@ -1,9 +1,9 @@
 class CreateTweets < ActiveRecord::Migration[5.2]
   def change
     create_table :tweets do |t|
-      t.string :name, null: false
-      t.string :text, null: false
-      t.text :image
+      t.string        :name, null: false
+      t.string        :text, null: false
+      t.text          :image
       t.timestamps
     end
   end
