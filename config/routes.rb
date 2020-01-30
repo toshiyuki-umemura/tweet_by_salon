f2b17b5c4db24ceeb017c2d623dee5dd3c266689
TatsuakiIto <akitatsu95.climbing@gmail.com>
2020-01-30 14:38:52 +0900

    create controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   root 'top#index'
   resources :top, only: [:index]
   resources :tweets, only: [:index]
+  resources :users, only: %i[new create]
+  resources :comments, only: %i[create destroy]
+  
 end
