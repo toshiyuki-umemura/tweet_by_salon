ecd0e797701511824015a0af606ed9e7ee36993f
chokobo-ume <@users.noreply.github.com>
2020-04-18 17:30:24 +0900

    ユーザー編集に伴う項目追加許可

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[name nickname])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :nickname])
   end
 
   private
